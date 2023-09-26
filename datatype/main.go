5acc843d095f25a4fc392751828e24d69419052a
Amodu Kehinde <amodukehindee@gmail.com>
2023-09-26 07:57:26 +0100

    first commit

---
+++ datatype/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String
+	var name string = "Hello world"
+	fmt.Println(name)
+
+	// Whole number
+	var number int = 14
+	fmt.Println(number)
+
+	// real numbers
+	var realNum float32 = 1.45
+	fmt.Println(realNum)
+
+	// Character
+	var ch rune = 'S'
+	fmt.Println(ch)
+}
---
+++ variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Kennis"
+	fmt.Println(name)
+
+	name = "Amodu Kehinde"
+	fmt.Println(name)
+}
